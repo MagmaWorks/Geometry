c3e75144ff97915325360e2fc6632022742793d7
kpne <kpn@kpne.dk>
2024-02-19 23:06:47 +0100

    polygon3d tests

--- GeometryTests/UnitTests/Polygon3dTests.cs
+++ GeometryTests/UnitTests/Polygon3dTests.cs
@@ -35,6 +35,33 @@ namespace GeometryTests.UnitTests
         }
 
         [Fact]
+        public void CastPolygonToLineTest()
+        {
+            // Assemble
+            var x1 = new Length(2.3, LengthUnit.Centimeter);
+            var y1 = new Length(5.4, LengthUnit.Centimeter);
+            var z1 = new Length(10, LengthUnit.Centimeter);
+            var x2 = new Length(-5.0, LengthUnit.Centimeter);
+            var y2 = new Length(7.1, LengthUnit.Centimeter);
+            var z2 = new Length(-2.5, LengthUnit.Centimeter);
+
+            // Act
+            IPoint3d pt1 = new Point3d(x1, y1, z1);
+            IPoint3d pt2 = new Point3d(x2, y2, z2);
+            var pts = new List<IPoint3d>() { pt1, pt2 };
+            var polygon = new Polygon3d(pts);
+            var ln = (Line3d)polygon;
+
+            // Assert
+            TestUtility.TestLengthsAreEqual(x1, ln.Start.X);
+            TestUtility.TestLengthsAreEqual(y1, ln.Start.Y);
+            TestUtility.TestLengthsAreEqual(z1, ln.Start.Z);
+            TestUtility.TestLengthsAreEqual(x2, ln.End.X);
+            TestUtility.TestLengthsAreEqual(y2, ln.End.Y);
+            TestUtility.TestLengthsAreEqual(z2, ln.End.Z);
+        }
+
+        [Fact]
         public void PolygonSurvivesJsonRoundtripTest()
         {
             // Assemble
