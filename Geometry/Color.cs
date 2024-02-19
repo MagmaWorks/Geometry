b4072c3acf30c3b139966dead4b0e1887f126926
Lint Action <lint-action@samuelmeuli.com>
2024-02-19 08:45:24 +0000

    Fix code style issues with dotnet_format

--- Geometry/Color.cs
+++ Geometry/Color.cs
@@ -5,11 +5,11 @@ namespace MagmaWorks.Geometry
     public class Color : IColor
     {
         public int ColorInt { get; }
-        public byte Alpha => (byte)((ColorInt >> (8 * 0)) & 0xff); 
+        public byte Alpha => (byte)((ColorInt >> (8 * 0)) & 0xff);
         public byte Red => (byte)((ColorInt >> (8 * 1)) & 0xff);
         public byte Green => (byte)((ColorInt >> (8 * 2)) & 0xff);
         public byte Blue => (byte)((ColorInt >> (8 * 3)) & 0xff);
-        
+
         public Color(byte alpha, byte red, byte green, byte blue)
         {
             ColorInt = BitConverter.ToInt32(new byte[] { alpha, red, green, blue }, 0);
